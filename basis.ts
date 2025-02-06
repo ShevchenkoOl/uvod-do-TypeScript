67262ec1e79cef701ba315af3d7055c986a743e6
Oleksii Shevchenko <105150555+ShevchenkoOl@users.noreply.github.com>
2025-02-06 15:22:33 +0100

    Update basis.ts

--- basis.ts
+++ basis.ts
@@ -84,6 +84,10 @@ arrNumber = [1, 5];
 
 let mixed: (number | string)[] = [1, 'two'];
 
+let mixed2: Array<number|string> = [1, 2, 3, "strg", 5];
+
+let number: number [] = [1, 2, 5]
+
 let users: {
     name: string;
     age: number;
@@ -106,3 +110,133 @@ let userOne: Users[] = [
 // or we can define absolutely any data type
 let arrAny: any[];
 arrAny = [123, 'TEXT', { name: 'Tom' }, [1, 2, 3]];
+
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+notSure = {};
+
+let num: number;
+
+num = notSure;
+
+// Unknown
+function fetchUserData() {
+  return 'Tom';
+}
+
+let userData: unknown = fetchUserData(); // fetchUserData повертає невідомі дані
+if (typeof userData === 'string') {
+  console.log(userData.toUpperCase()); // OK, тепер ми знаємо, що це рядок
+}
+
+
+// Tuple
+// Кортеж, TypeScript це тип даних, що дозволяє визначити масив з фіксованою кількістю елементів, типи яких відомі, але не обов'язково повинні бути однаковими.
+let tupleType: [string, boolean];
+tupleType = ['hello', true]; // OK
+tupleType = [true, 'hello']; // Error. Неправильні типи
+tupleType = ['hello', true, true]; // Error. Більше значень ніж у tuple
+// Кортежі зручні, коли нам потрібно зберегти в масив фіксовані значення, наприклад, день, місяць та рік.
+let date: [number, string, number];
+date = [02, "JUN", 2025];
+
+// але метод push спрацьовує вірно:
+let fixed: [string, number];
+fixed = ['Text', 10];
+fixed.push('Add this text');
+console.log(fixed);  //  ["Text", 10, "Add this text"]
+// але якщо ми виводимо в консоль тип останнього додатного елемента, то TS віведет помилку:
+console.log(typeof fixed[2]) // Tuple type '[string, number]' of length '2' has no element at index '2'.
+
+
+//TS може також і розширити кортеж:
+let tuple: [string, ...number[]];
+tuple = ['hello', 42, 100, 200];
+
+let newTuple: [string, number, ...string[]];
+newTuple = ["red", 15, "blue", "home"];
+
+
+
+// enum, это специальный тип, который позволяет задавать **набор связанных значений** с понятными именами. TypeScript сам присваивает числовые значения (по умолчанию с 0).
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+console.log(Color.Red); // 0
+console.log(Color.Green); // 1
+console.log(Color.Blue); // 2
+
+//Можно задать свои значения:  
+  enum Status {
+    Success = 200,
+    NotFound = 404,
+    ServerError = 500
+  }
+  console.log(Status.NotFound); // 404
+
+ // const enum — облегчённая версия
+const enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+console.log(Direction.Up); // 0 const enum` полностью исчезает при компиляции!  
+
+
+
+// Union Type у TypeScript дозволяє вказати, що значенням може бути один із кількох типів:
+let mixedType: string | number | boolean;
+mixedType = 'string'; // OK
+mixedType = 10; // OK
+mixedType = true; // OK
+
+//Union Type працює не лише з базовими типами, а й з об'єктами:
+type Dog = { 
+  legs: 4;
+  bark: () => void;
+}
+
+type Fish = {
+  fins: 2;
+  swim: () => void;
+}
+
+let pet: Dog | Fish; // може бути або об'єктом типу Dog, або об'єктом типу Fish.
+
+
+
+// Intersection type є способом об'єднання декількох типів в один. Це дозволяє створювати складні типи, комбінуючи прості
+type Employee = {
+  name: string;
+  id: number;
+};
+
+type Manager = {
+  employees: Employee[];
+};
+
+type CEO = Employee & Manager;
+
+const newObject: CEO = {
+  name: 'Alice',
+  id: 1,
+  employees: [
+    {
+      name: 'Bob',
+      id: 2,
+    },
+  ],
+};
+
+
+
+// Literal Type — це тип, що набуває конкретного значення. З ним ви можете визначити тип змінної так, щоб він набував лише певних значень.
+type OneOrTwo = 1 | "red";
+let value: OneOrTwo;
+value = 1; // OK
+value = "red"; // OK
+value = 3; // Error: Type '3' is not assignable to type 'OneOrTwo'.
